be51139bbd58a1a9c688751e9eab1ea89b85da5f
Alexandre Pierre <alexandre.pierre@student-cs.fr>
2022-10-25 14:28:51 +0200

    First similarity function

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-    // return new Response("Hello World");
-    return new Response(JSON.stringify(_req))
+    return new Response(similarity("Hello ", " world !"))
 }
+
+function similarity(word1: string, word2:string): string {
+    return word1+word2;
+}
+
 
 serve(handler);
