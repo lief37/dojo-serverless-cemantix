749612b8fdca4ff6675a2892c791f929dc205aae
Alexandre Pierre <alexandre.pierre@student-cs.fr>
2022-10-25 14:22:49 +0200

    test

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-    console.log(_req);
-    return new Response("Hello World");
+    // return new Response("Hello World");
+    return new Response(_req)
 }
 
 serve(handler);
