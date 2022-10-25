bb4f0f955da85ce6fd5ce3f2fc0c8f50e60ea4cd
Alexandre Pierre <alexandre.pierre@student-cs.fr>
2022-10-25 15:02:43 +0200

    test

--- index.ts
+++ index.ts
@@ -1,13 +1,29 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
+import { get } from 'axios';
 
 async function handler(_req: Request): Promise<Response> {
     const guess = await extractGuess(_req);
-    return new Response(similarity("This is your guess: ", guess))
+    const simscore = await getResponse('','');
+    return new Response(similarity(`This is your guess: ${guess}. `, `This is your similarity ${simscore}.` ))
 }
 
 function similarity(word1: string, word2:string): string {
     return word1+word2;
 }
+
+
+const getResponse = async (word1: string, word2:string) => {
+    const body = {
+        lang: 'fr',
+        sim1: 'chat',
+        sim2: 'chien',
+        type: 'General Word2Vec'
+    }
+
+    const JSON_response = await get('http://nlp.polytechnique.fr/similarityscore', JSON.stringify(body));
+    return JSON.parse(JSON_response).simscore;
+}
+
 
 const extractGuess = async (req: Request) => {
     const slackPayload = await req.formData();
